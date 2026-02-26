4805c28c7b6ccd1990915a2c6c138d22af54fede
zheithi <a.sussy.business@gmail.com>
2026-02-26 11:03:08 +0100

    test

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -1,14 +1,9 @@
-
-
 public class Main {
     public static void main(String[] args) {
 
-        System.out.print("Hello and welcome!");
+        System.out.println("Hello world!");
+        String foid = "Test";
+        System.out.println(foid);
 
-        for (int i = 1; i <= 5; i++) {
-
-            System.out.println("i = " + i);
-
-        }
     }
 }
